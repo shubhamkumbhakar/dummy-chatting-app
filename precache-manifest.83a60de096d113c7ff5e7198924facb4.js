74802a00216db67a74cc0e72c47a0ceb88149e55
shubhamkumbhakar <shubhamkumbhakar27@gmail.com>
2020-08-19 02:17:53 +0530

    Updates

---
+++ precache-manifest.83a60de096d113c7ff5e7198924facb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75beb87301e128bc5e55a137d21d1776",
+    "url": "/dummy-chatting-app/index.html"
+  },
+  {
+    "revision": "a7a9f4a103b6518e4c01",
+    "url": "/dummy-chatting-app/static/css/main.a93c3734.chunk.css"
+  },
+  {
+    "revision": "03330c0ddfb9e35f16d5",
+    "url": "/dummy-chatting-app/static/js/2.d83e14cf.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/dummy-chatting-app/static/js/2.d83e14cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7a9f4a103b6518e4c01",
+    "url": "/dummy-chatting-app/static/js/main.cf573ffc.chunk.js"
+  },
+  {
+    "revision": "317f401440d5d42d0aad",
+    "url": "/dummy-chatting-app/static/js/runtime-main.24f0cf67.js"
+  }
+]);
